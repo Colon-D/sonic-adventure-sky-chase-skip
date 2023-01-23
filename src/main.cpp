e95946668eee4e7d443c6340259f3cd3ed9f875a
Colon-D <JJPTails@gmail.com>
2023-01-23 17:24:27 +0000

    replace all assembly with higher level code
    This makes the code easier to read.
    This also no longer skips the dialogue when the Tornado transforms.
    Finally, the Tornado's position is set after the Tornado is boosted,
    meaning that the Tornado draws properly instead of lagging behind.

--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,92 @@
 #include "SADXModLoader.h"
 
-/// whether "whatever B is" is being held.
-/// ideally this would be X as X does not fire, but X seems to be Y and Y seems
-/// to be L and idk what L is. moral of the story, B is the button
-bool holding_b{ false };
+// todo config
+constexpr auto button = Buttons::Buttons_B;
+constexpr unsigned speed{ 15 };
+
+bool try_boost{ false };
 
 /// the tornado progress variable
 DataPointer(unsigned, tornado, 0x3C82300);
-
-/// if "whatever B is" is being held, move the tornado an 16x faster
-void progress_sky_chase() {
-	if (holding_b) {
-		tornado += 15;
-	}
-}
-
-/// location to jump to after progress_sky_chase_asm()
-const unsigned* loc_626E78{ reinterpret_cast<unsigned*>(0x00626E78) };
-/// tornado movement for sky chase act 1
-void __declspec(naked) progress_sky_chase_asm() {
-	__asm {
-		; execute original instructions
-		mov     eax, tornado          ; get the tornado pointer
-		inc     DWORD PTR [eax]       ; move tornado forward
-		pushad                        ; push current registers, so that
-		                              ; my function does not mess with them
-		call    progress_sky_chase    ; call my function
-		popad                         ; pop registers
-		mov     eax, [eax]            ; set eax to tornado value
-		add     edx, 0FFFFFF9Ch       ; subtract 100 from something
-		cmp     eax, edx              ; compare tornado to that something
-		; and finally, return back to the original code
-		jmp     [loc_626E78]          ; go back!
-	}
-}
-
-/// location to jump to after progress_sky_chase_2_asm()
-const unsigned* loc_627187{ reinterpret_cast<unsigned*>(0x00627187) };
-/// tornado movement for sky chase act 2, before transformation
-void __declspec(naked) progress_sky_chase_2_asm() {
-	__asm {
-		; execute original instructions
-		mov     eax, tornado
-		inc     DWORD PTR [eax]
-		; call my function
-		pushad
-		call    progress_sky_chase
-		popad
-		; go back!
-		mov     eax, [eax]
-		cmp     eax, 157Ch
-		jmp     [loc_627187]                  
-	}
-}
-
-/// location to jump to after progress_sky_chase_2_2_asm()
-const unsigned* loc_6272CE{ reinterpret_cast<unsigned*>(0x6272CE) };
-DataPointer(unsigned, dword_29E345C, 0x29E345C);
-/// tornado movement for sky chase act 2, after transformation
-void __declspec(naked) progress_sky_chase_2_2_asm() {
-	__asm {
-		; execute original instructions
-		mov     eax, tornado
-		inc     DWORD PTR[eax]
-		; call my function
-		pushad
-		call    progress_sky_chase
-		popad
-		; go back!
-		mov     eax, [eax]
-		mov     ecx, [dword_29E345C]
-		add     [ecx], 0FFFFFFFBh
-		cmp     eax, [ecx]
-		jmp     [loc_6272CE]
-	}
-}
+/// the tornado's current position
+DataPointer(NJS_VECTOR, tornado_translation, 0x3C822B4);
+/// array of positions tornado should be in during the level
+DataPointer(NJS_VECTOR*, tornado_translation_array, 0x3C822A0);
 
 extern "C" {
-	__declspec(dllexport) void __cdecl Init(
-		const char* path,
-		const HelperFunctions& helper_functions
-	) {
-		// tornado movement for sky chase act 1
-		WriteJump(
-			reinterpret_cast<void*>(0x626E6D),
-			progress_sky_chase_asm
-		);
-		// tornado movement for sky chase act 2, before transformation
-		WriteJump(
-			reinterpret_cast<void*>(0x627177),
-			progress_sky_chase_2_asm
-		);
-		// tornado movement for sky chase act 2, after transformation
-		WriteJump(
-			reinterpret_cast<void*>(0x6272B8),
-			progress_sky_chase_2_2_asm
-		);
+	__declspec(dllexport) void __cdecl OnInit(
+		const char* const /*path*/, const HelperFunctions& /*helper_functions*/
+	) {}
+
+	__declspec(dllexport) void __cdecl OnControl() {
+		// if in Sky Chase and Boost button is being held
+		if (
+			(
+				CurrentLevel == LevelIDs::LevelIDs_SkyChase1
+				|| CurrentLevel == LevelIDs::LevelIDs_SkyChase2
+			)
+			&& (Controllers[0].HeldButtons & button || Controllers[0].PressedButtons & button)
+		) {
+			// try to boost
+			try_boost = true;
+
+			// disable the boost buttons
+			Controllers[0].HeldButtons &= ~button;
+			Controllers[0].PressedButtons &= ~button;
+		}
+		else {
+			try_boost = false;
+		}
 	}
 
-	__declspec(dllexport) void __cdecl OnInput() {
-		// check if "whatever B is" is being held
-		holding_b = ControllerPointers[0]->HeldButtons & Buttons_B;
+	__declspec(dllexport) void __cdecl OnFrame() {
+		switch (CurrentLevel) {
+		case LevelIDs_SkyChase1: {
+			if (!try_boost) {
+				break;
+			}
+			// if boosting in sky chase zone 1:
+			// stop at end cutscene
+			constexpr unsigned end{ 8440 };
+			if (tornado < end) {
+				tornado += speed;
+				if (tornado > end) {
+					tornado = end;
+				}
+			}
+			break; }
+		case LevelIDs_SkyChase2: {
+			if (!try_boost) {
+				break;
+			}
+			// if boosting in sky chase zone 2:
+			// when you have reached boss you are teleported to 15,
+			// this check prevents you from moving whilst fighting the boss
+			constexpr unsigned boss{ 15 };
+			constexpr unsigned transform_begin{ 5000 };
+			constexpr unsigned transform_end{ 5500 };
+			if (tornado > boss) {
+				// the transformation dialogue begins here
+				if (tornado < transform_begin) {
+					tornado += speed;
+					if (tornado > transform_begin) {
+						tornado = transform_begin;
+					}
+				}
+				// the transformation has ended here
+				if (tornado > transform_end) {
+					tornado += speed;
+				}
+			}
+			break; }
+		default:
+			// return if not in Sky Chase Zone
+			return;
+		}
+		// if in either Sky Chase Zone, display the tornado at right position
+		if (PlayerPtrs[0]) {
+			tornado_translation = tornado_translation_array[tornado];
+		}
 	}
 
 	__declspec(dllexport) ModInfo SADXModInfo{ ModLoaderVer };
